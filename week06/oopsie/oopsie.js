3c5a09e23849aa8c619a46122558e88ab8cee341
NNRIN <nicknoah.rindlisbacher@gmail.com>
2025-03-30 18:29:52 +0200

    finished oopsie (sort of)

--- week06/oopsie/oopsie.js
+++ week06/oopsie/oopsie.js
@@ -7,11 +7,16 @@
 //   fallback()      // "oopsie": go back to last start (fallback position)
 //   turn()          // cash in your win, update fallback position for next turn
 //
-function Player() {
+const goal = 99;
+
+function Player(c) {
     return {
         fallbackIndex : 0,
         progressIndex : 0,
+        color: c,
 
+        getColor : function(){
+            return this.color},
         getFallbackIndex : function() {return this.fallbackIndex},
         getProgressIndex : function() {return this.progressIndex},
         proceed : function(stride){this.progressIndex += stride},
@@ -19,6 +24,28 @@ function Player() {
         turn : function(){this.fallbackIndex = this.progressIndex},
     }
 }
+
+function GameState() {
+    return {
+        activePlayer : player1,
+        endmsg: "game over, press f5 to restart",
+        gameOver: false,
+
+        switchTurn : function() {
+            if (this.activePlayer === player1) {
+                this.activePlayer = player2;
+            } else {
+                this.activePlayer = player1;
+            }
+        },
+        getPlayer: function (){return this.activePlayer},
+        endGame: function(){this.gameOver = true},
+    }
+}
+player1 = Player("blue");
+player2 = Player("red");
+
+gameState = GameState();
 
 
 function start() {
@@ -34,18 +61,36 @@ function start() {
 }
 
 function dice() {
+    if (gameState.gameOver == true) {
+        return;
+    }
     const stride                              = Math.round(1 + Math.random() * 5);
     document.getElementById('dice').innerText = ""+ stride;
+    let currentPlayer = gameState.getPlayer(); // Cache the current player
+
     if (stride === 3) {
-        player.fallback();
+        currentPlayer.fallback();
+        //gameState.switchTurn(); // Move switchTurn after the winner check
     } else {
-        player.proceed(stride);
+        currentPlayer.proceed(stride);
+    }
+
+    if (currentPlayer.getProgressIndex() >= goal) {
+        declareWinner(currentPlayer);
+    }
+
+    if (stride === 3){
+        gameState.switchTurn();
     }
     display();
 }
 
 function turn() {
-    player.turn();
+    if (gameState.gameOver == true) {
+        return;
+    }
+    gameState.getPlayer().turn();
+    gameState.switchTurn()
     display();
 }
 
@@ -54,10 +99,24 @@ function display() {
         const field = document.getElementById("FIELD-" + i);
         field.setAttribute("CLASS", "field");
     }
-    const fallbackField = document.getElementById("FIELD-" + player.getFallbackIndex());
-    fallbackField.setAttribute("CLASS", "field fallback");
-    const progressField = document.getElementById("FIELD-" + player.getProgressIndex());
-    progressField.setAttribute("CLASS", "field progress");
+
+    if (player1.fallbackIndex === player2.fallbackIndex && player1.getProgressIndex() === player2.getProgressIndex()) {
+        let fallbackField = document.getElementById("FIELD-" + player1.getFallbackIndex());
+        fallbackField.setAttribute("CLASS", "field bothOnPos");
+    }else {
+        const fallbackField = document.getElementById("FIELD-" + player1.getFallbackIndex());
+        fallbackField.setAttribute("CLASS", "field fallback");
+        const progressField = document.getElementById("FIELD-" + player1.getProgressIndex());
+        progressField.setAttribute("CLASS", "field progress");
+        const fallbackField1 = document.getElementById("FIELD-" + player2.getFallbackIndex());
+        fallbackField1.setAttribute("CLASS", "field fallbackInactive");
+        const progressField1 = document.getElementById("FIELD-" + player2.getProgressIndex());
+        progressField1.setAttribute("CLASS", "field progressInactive");
+    }
 }
 
-player = Player();
+function declareWinner(winner) {
+    document.getElementById('goal').innerText = "winner "+ winner.getColor() + " " + gameState.endmsg;
+    gameState.endGame();
+}
+
